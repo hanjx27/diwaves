8e3955f7b10d90c7ac95ce3c785ad91a36fc5e36
hanjx27 <hanjx27@chinaunicom.cn>
2020-04-29 10:45:44 +0800

    update

--- src/utils/Global.js
+++ src/utils/Global.js
@@ -3,8 +3,8 @@
 //exports.baseurl = 'http://192.168.1.6:8080/IComment/';
 //exports.baseimgurl = 'http://192.168.1.6:8080/IComment/upload/';
 
-exports.baseurl = 'http://localhost:8080/IComment/';
-exports.baseimgurl = 'http://localhost:8080/IComment/upload/';
+//exports.baseurl = 'http://localhost:8080/IComment/';
+//exports.baseimgurl = 'http://localhost:8080/IComment/upload/';
 
-//exports.baseurl = 'http://www.shzihai.com/IComment/';
-//exports.baseimgurl = 'http://www.shzihai.com/IComment/upload/';
+exports.baseurl = 'http://www.shzihai.com/IComment/';
+exports.baseimgurl = 'http://www.shzihai.com/IComment/upload/';
